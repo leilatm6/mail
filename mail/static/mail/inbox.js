e217ded2e6b55206df9956b31d6ac523e14d871a
leila <leilatm6@hotmail.com>
2023-10-06 12:20:24 +0400

    view email done

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   
-  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -78,36 +77,96 @@ function load_mailbox(mailbox) {
    else{
     const List = document.createElement('ul');
     List.setAttribute('class', "list-group");
+    List.id= "mailboxlist";
     document.querySelector('#emails-view').appendChild(List);
-    
     for(let email of emails){
       const listItem = document.createElement('li');
-      listItem.classList.add("list-group-item","mailbox-item", email.read ? "read":"unread");    
+      listItem.classList.add("list-group-item","mailbox-item", email.read ? "read":"unread"); 
+      listItem.dataset.emailid = email.id   
       listItem.innerHTML = `<div class="mailbox-sender">${email.sender}</div>
       <div class="mailbox-subject">${email.subject}</div>
       <div class=" mailbox-timestamp"><small> ${email.timestamp}</small></div>`
       List.appendChild(listItem);
     }
-
-
-
+    const mailBoxList =document.querySelector("#mailboxlist")
+    mailBoxList.addEventListener('click',(event)=>{
+      const clickedLi = event.target.closest('li');
+      if (clickedLi){
+        console.log('Clicked on:', clickedLi.textContent);
+        load_email(clickedLi.dataset.emailid);
+      } 
+ });
    }
  });
+ 
+// document.querySelector("#mailboxlist").addEventListener('mousemove',(event)=>{
+//});
 }
-
 
 function load_email(emailid){
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-  fetch(`/emails/${emailid}`)
+fetch(`/emails/${emailid}`)
 .then(response => response.json())
 .then(email => {
     // Print email
     console.log(email);
-    if "error"
+    const emailData = {
+      subject: email.subject,
+      sender: email.sender,
+      body: email.body,
+      time: email.timestamp
+  };
+    emailshow(emailData);
+    fetch(`/emails/${emailid}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    })
 
-    // ... do something else with email ...
 });
+}
+
+
+
+
+function emailshow(emailData){
+
+emailView = document.querySelector('#email-view');
+emailView.innerHTML = "";
+// Create email container
+const emailContainer = document.createElement('div');
+emailContainer.classList.add('email-container');
+
+// Create email subject element
+const emailSubject = document.createElement('div');
+emailSubject.classList.add('email-subject');
+emailSubject.textContent = emailData.subject;
+
+// Create email sender element
+const emailSender = document.createElement('div');
+emailSender.classList.add('email-sender');
+emailSender.textContent = emailData.sender;
+
+// Create email body element
+const emailBody = document.createElement('div');
+emailBody.classList.add('email-body');
+emailBody.textContent = emailData.body;
+
+// Create email time element
+const emailTime = document.createElement('div');
+emailTime.classList.add('email-time');
+emailTime.textContent = emailData.time;
+
+// Append elements to the email container
+emailContainer.appendChild(emailSubject);
+emailContainer.appendChild(emailSender);
+emailContainer.appendChild(emailBody);
+emailContainer.appendChild(emailTime);
+
+// Add the email container to the document
+emailView.appendChild(emailContainer);
 }
